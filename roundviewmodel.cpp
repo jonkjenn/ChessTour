5f057d6fc5fb680542ace354e4e14ebff4283b5f
Jon Kjennbakken <jonkjenn@gmail.com>
2017-08-22 13:20:07 +0200

    Added viewmodel for each round in a tournament

---
+++ roundviewmodel.cpp
@@ -0,0 +1,6 @@
+#include "roundviewmodel.h"
+
+RoundViewModel::RoundViewModel()
+{
+
+}
---
+++ roundviewmodel.h
@@ -0,0 +1,11 @@
+#ifndef ROUNDVIEWMODEL_H
+#define ROUNDVIEWMODEL_H
+
+
+class RoundViewModel
+{
+public:
+    RoundViewModel();
+};
+
+#endif // ROUNDVIEWMODEL_H
